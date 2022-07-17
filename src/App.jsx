dce1b9d825be00c379157d74abb92062c03697e4
Ariel <caminosariel@gmail.com>
2022-07-16 23:21:26 -0300

    first use useStates

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import Formulario from '../Formulario'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [titulo, setTitulo] = useState('clase de react')
+  const [texto, setTexto] = useState('')
 
   return (
     <div className="App">
-      <Formulario/>
+      {/* <Formulario/> */}
+      <h1>{titulo}</h1>
+      <p>Contenido de clase {texto}</p>
+      <button onClick={()=>setTexto('2021')}>Agregar</button>
+      <button onClick={()=>setTexto('')}>Limpiar</button>
+      <button onClick={()=>setTitulo('Nuevo titulo')}>Nuevo titulo</button>
     </div>
   )
 }
