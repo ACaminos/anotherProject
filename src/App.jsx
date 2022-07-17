0f42470f4109d44e980a8d6b5eccd9b9b80dc283
Ariel <caminosariel@gmail.com>
2022-07-17 19:54:53 -0300

    use fetch API Mercado libre

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
-import { useState } from 'react'
-import Formulario from '../Formulario'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [titulo, setTitulo] = useState('clase de react')
-  const [texto, setTexto] = useState('')
 
-  return (
-    <div className="App">
-      {/* <Formulario/> */}
-      <h1>{titulo}</h1>
-      <p>Contenido de clase {texto}</p>
-      <button onClick={()=>setTexto('2021')}>Agregar</button>
-      <button onClick={()=>setTexto('')}>Limpiar</button>
-      <button onClick={()=>setTitulo('Nuevo titulo')}>Nuevo titulo</button>
-    </div>
+  const [productos, setProductos] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(
+    ()=>{
+      fetch("https://api.mercadolibre.com/sites/MLA/search?q=ipod")
+      .then(res=>res.json())
+      .then(data =>{
+        console.log(data)
+        if(data.results){
+          setProductos(data.results)
+          setLoading(false)
+        }
+      })
+    },[]
   )
-}
+
+  if(loading){
+    return(
+      <div>Loading...</div>
+    )
+  }
+    else{
+      return(
+        <>
+          Listado de productos
+          {productos.map( producto =>
+            <>
+              <p>{producto.title}</p>
+              <p>{producto.price}</p> 
+            </>
+            )}
+        </>
+      )
+    }
+  }
 
 export default App
